f20b10682b7202f20f10095ea0c9f6ed5ebc554c
thakurajeet99@gmail.com <thakurajeet99@gmail.com>
2018-06-07 14:38:25 +0300

    Modified code to accurate test.

--- src/app/containers/users/users.component.spec.ts
+++ src/app/containers/users/users.component.spec.ts
@@ -1,5 +1,4 @@
-
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, discardPeriodicTasks, tick } from '@angular/core/testing';
 import { UsersComponent } from './users.component';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { UserService } from '../../services/user.service';
@@ -7,6 +6,9 @@ import { HttpClient } from '@angular/common/http';
 import { of } from "rxjs/observable/of";
 import { timer } from "rxjs/observable/timer";
 import { mapTo } from "rxjs/operators";
+import { UserListComponent } from "../../components/user-list/user-list.component";
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from "@angular/platform-browser";
 
 describe('UsersComponent', () => {
   let component: UsersComponent;
@@ -16,12 +18,12 @@ describe('UsersComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UsersComponent ],
+      imports:[RouterTestingModule],
+      declarations: [ UsersComponent,UserListComponent ],
       providers: [UserService,
         { provide: HttpClient, useValue: {} },
       ],
-
-      schemas:[NO_ERRORS_SCHEMA]
+      // schemas:[NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -46,6 +48,34 @@ describe('UsersComponent', () => {
       done();
     })
   });
+  it('should have a list of users', fakeAsync(() => {
+    const spy = spyOn(userService,'getUsers').and.returnValue(timer(1000).pipe(mapTo([fakeUser])));
+    component.ngOnInit();
+
+    component.users$.subscribe(users => {
+      console.log(document.querySelector('app-user-list').innerHTML);
+      expect(users).toEqual([fakeUser]);
+    });
+    tick(1000);
+    discardPeriodicTasks();
+  }));
+  it('should have a list of users1', async(() => {
+    const spy = spyOn(userService,'getUsers').and.returnValue(timer(1000).pipe(mapTo([fakeUser])));
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    component.users$.subscribe(users => {
+      //console.log(document.querySelector('app-user-list').innerHTML);
+      expect(users).toEqual([fakeUser]);
+    });
+    //tick(1000);
+    //discardPeriodicTasks();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      const buttons = fixture.debugElement.queryAll(By.css('.user-button'));
+      expect(buttons[0].nativeElement.textContent).toEqual('fake');
+    })
+  }));
 });
 
 
